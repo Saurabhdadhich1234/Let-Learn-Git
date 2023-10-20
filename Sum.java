62a409248d45adc8fd80382591a54c89dcdfce40
Saurabh Dadhich <saurabhdadhich4567@gmail.com>
2023-10-20 07:04:30 +0530

    adding space

--- Sum.java
+++ Sum.java
@@ -1,7 +1,9 @@
 public class Sum{
     public static void main(String args[]){
-        int a=5,b=5,sum=0;
+        int a=5,b=5;
+        int  sum=0;
         sum=a+b;
+        
         System.out.println("This is the sum="+sum);
     }
 }
